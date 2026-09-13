1140f8415311f0093cd6f86778c890474711b45e
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-09-13 19:43:54 -0300

    Tipos primitivos pt 05 - Exercício

---
+++ src/academy/devdojo/maratonajava/Aula03TiposExercicio.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava;
+/*
+ prática
+
+ crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+ eu <nome>, morando em <endereço>,
+ confirmo que recebi o salário de <salario>, na data <data>
+*/
+
+public class Aula03TiposExercicio {
+    public static void main(String[] args) {
+        String nome = "Otávio";
+        String endereco = "Joinville";
+        float salario = 2500;
+        String data = "13/09/2026";
+
+        System.out.println("eu "+nome+", morando em "+endereco+" "
+                + "comfirmo que recebi o salário de "+salario+", na data "+data);
+
+    }
+}
