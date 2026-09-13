a996c51d93a850a3ccd4809eab126e1c9dec5ffb
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-09-13 10:38:56 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+         /*
+        Tipos de dados primitivos:
+        byte - 8 bits - valores de -128 a 127 - 1 byte
+        short - 16 bits - valores de -32.768 a 32767 - 2 bytes
+        int - 32 bits - valores de -2^32 a 2^32 - 1 - 4 bytes
+        long - 64 bits - valores de -2^63 a 2^63 - 1 - requer sufixo L - 8 bytes
+        float - 32 bits - precisão simples - requer sufixo f - 4 bytes
+        double - 64 bits - precisão dupla - padrão para números decimais - 8 bytes
+        char - 16 bits - armazena 1 caractere - 2 bytes
+        boolean - 1 bit - guarda false or true - 1 bit
+         */
+        int ageInt = 10;
+        long bigNumber =1000000;
+        char letter = 'M';
+        byte ageByte = 10;
+        short ageShort = 10;
+        double salaryDouble = 2500;
+        float salaryFloat = 2500;
+        boolean truth = true;
+        boolean fake = true;
+        System.out.println("age is "+ageInt+ " years");
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+//comentario
+
+/*
+
+  *isso é um <b>comentario</b>
+
+*/
+
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
