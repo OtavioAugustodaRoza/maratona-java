4a8da31ebe56d749e70b1ce81cae4cde2efc589a
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-09-13 21:54:48 -0300

    Operadores pt 01 - Aritiméticos

---
+++ src/academy/devdojo/maratonajava/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+
+    }
+}
