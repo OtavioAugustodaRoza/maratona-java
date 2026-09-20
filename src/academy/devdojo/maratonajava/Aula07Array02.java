ccbe1541a3d1ca137a93bc90fe0a2e8af9bb73bd
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-09-20 20:12:26 -0300

    Arrays pt 02

---
+++ src/academy/devdojo/maratonajava/Aula07Array02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava;
+
+public class Aula07Array02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double 0
+        // char '/u000'''
+        //boolean false
+        //String null
+
+        char [] idades = new char[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
